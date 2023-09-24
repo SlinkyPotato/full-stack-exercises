e62bdf91d9d1f2323e5759f949f6e3dc64f1b7e7
Brian Patino <Patinobrian@gmail.com>
2023-09-24 14:18:37 -0400

    complete exercises 4.3-4.5

---
+++ part4/bloglist/utils/list_helper.js
@@ -0,0 +1,26 @@
+
+const dummy = (blogs) => {
+  return 1;
+};
+
+const totalLikes = (blogs) => {
+  const reducer = (sum, item) => {
+    return sum + item.likes;
+  };
+
+  return blogs.reduce(reducer, 0);
+};
+
+const favoriteBlog = (blogs) => {
+  const reducer = (favorite, item) => {
+    return favorite.likes > item.likes ? favorite : item;
+  };
+
+  return blogs.reduce(reducer, {});
+};
+
+module.exports = {
+  dummy,
+  totalLikes,
+  favoriteBlog,
+};
