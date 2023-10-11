0e23b22d90ac603946300da8686a2f6abf9aa497
Brian Patino <Patinobrian@gmail.com>
2023-10-11 12:04:02 -0400

    wip

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -1,9 +1,13 @@
 import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
+import LoginForm from './components/LoginForm'
 
 const App = () => {
-  const [blogs, setBlogs] = useState([])
+  const [blogs, setBlogs] = useState([]);
+  const [user, setUser] = useState(null);
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -12,12 +16,13 @@ const App = () => {
   }, [])
 
   return (
-    <div>
+    <>
+      
       <h2>blogs</h2>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
-    </div>
+    </>
   )
 }
 
--- part5/bloglist-frontend/src/services/blogs.js
+++ part5/bloglist-frontend/src/services/blogs.js
@@ -1,9 +1,32 @@
 import axios from 'axios'
-const baseUrl = '/api/blogs'
+const baseUrl = import.meta.env.VITE_BACKEND_HOST + '/api/blogs'
 
-const getAll = () => {
-  const request = axios.get(baseUrl)
-  return request.then(response => response.data)
-}
+const getAll = async () => {
+  try {
+    const response = await axios.get(baseUrl)
+    return response.data
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+};
 
-export default { getAll }
+const setToken = (newToken) => {
+  axios.defaults.headers.common['Authorization'] = `bearer ${newToken}`
+};
+
+const create = async (newObject) => {
+  try {
+    const response = await axios.post(baseUrl, newObject)
+    return response.data
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+};
+
+export default {
+  getAll,
+  setToken,
+  create
+};
